72c126228d7558a61891e1936f04fc9b4e24f439
jpmoraess <moraesdeveloperweb@gmail.com>
2022-01-28 22:58:48 -0300

    Criando consumidores em Java

---
+++ src/main/java/br/com/moraesit/ecommerce/FraudDetectorService.java
@@ -0,0 +1,51 @@
+package br.com.moraesit.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class FraudDetectorService {
+
+    public static void main(String[] args) {
+        var consumer = new KafkaConsumer<String, String>(properties());
+        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
+        while (true) {
+            var records = consumer.poll(Duration.ofMillis(100));
+            if (!records.isEmpty()) {
+                System.out.println("I've found " + records.count() + " records..");
+
+                for (var record : records) {
+                    System.out.println("-----------------------------------------");
+                    System.out.println("Processing new order, checking for fraud.");
+                    System.out.println("key: " + record.key());
+                    System.out.println("value: " + record.value());
+                    System.out.println("partition: " + record.partition());
+                    System.out.println("offset: " + record.offset());
+                    System.out.println("-----------------------------------------");
+
+                    try {
+                        TimeUnit.SECONDS.sleep(4);
+                    } catch (InterruptedException e) {
+                        // ignoring
+                        e.printStackTrace();
+                    }
+                    System.out.println("Order processed!!!");
+                }
+            }
+        }
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+        return properties;
+    }
+}
